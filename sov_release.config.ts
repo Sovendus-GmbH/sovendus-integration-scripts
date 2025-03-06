a94c1e2b7bb06aade64ef0b67950472583310a43
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2025-03-06 13:02:51 +0100

    chore: update integration script version to 3.4.0 and bump dependencies

--- sov_release.config.ts
+++ sov_release.config.ts
@@ -1,4 +1,4 @@
-import type { ReleaseConfig } from "sovendus-release-tool/types";
+import type { ReleaseConfig } from "sovendus-release-tool";
 
 const releaseConfig: ReleaseConfig = {
   packages: [
@@ -7,6 +7,16 @@ const releaseConfig: ReleaseConfig = {
       updateDeps: true,
       version: "3.4.0",
       release: true,
+      lintAndBuild: true,
+      test: true,
+      versionBumper: {
+        jsVars: [
+          {
+            filePath: "wordpress-plugin/wc-sovendus.php",
+            varName: "SOVENDUS_VERSION",
+          },
+        ],
+      },
       releaseOptions: {
         // foldersToScanAndBumpThisPackage: [
         //   // scan whole dev env
--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,1 @@
-// export const optimizeDomain = "https://www.sovopt.com/";
-
-export const integrationScriptVersion = "TODO"; // TODO get version from package.json
+export const integrationScriptVersion = "3.4.0";
