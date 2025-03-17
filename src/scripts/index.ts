09c444c70538cacb55e3a7801b8497f67b30bca5
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2025-03-17 13:09:17 +0100

    Add utils export from landing-page to index

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,4 +1,5 @@
 export * from "./constants";
 export * from "./landing-page/sovendus-page-handler";
+export * from "./landing-page/utils";
 export * from "./shared-utils";
 export * from "./thankyou-page/thankyou-page-handler";
