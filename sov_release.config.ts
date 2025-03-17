539cef75dcac17e4aa3d30c4ae49897fec70e5a2
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2025-03-17 13:09:57 +0100

    Release: 3.5.5

--- sov_release.config.ts
+++ sov_release.config.ts
@@ -9,7 +9,7 @@ const releaseConfig: ReleaseConfig = {
       build: true,
       test: true,
       release: {
-        version: "3.5.4",
+        version: "3.5.5",
         versionBumper: [
           {
             filePath: "src/scripts/constants.ts",
--- src/scripts/constants.ts
+++ src/scripts/constants.ts
@@ -1,1 +1,1 @@
-export const integrationScriptVersion = "3.5.4";
+export const integrationScriptVersion = "3.5.5";
