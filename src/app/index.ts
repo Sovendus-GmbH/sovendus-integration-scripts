b48918008ae8472ff9a4ffb15832ed53301b7519
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2025-04-14 17:37:18 +0200

    fix exports

---
+++ src/app/index.ts
@@ -0,0 +1,13 @@
+import SovendusLandingPageDemoForm from "./(websiteMock)/landing-page/demo-form";
+import SovendusLandingPageDemo from "./(websiteMock)/landing-page/page";
+import EmptyDemoPage from "./(websiteMock)/page";
+import { SovendusThankyouPageDemoForm } from "./(websiteMock)/thank-you/demo-form";
+import SovendusThankYouPageDemo from "./(websiteMock)/thank-you/page";
+
+export {
+  EmptyDemoPage,
+  SovendusLandingPageDemo,
+  SovendusLandingPageDemoForm,
+  SovendusThankYouPageDemo,
+  SovendusThankyouPageDemoForm,
+};
